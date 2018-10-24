9be56bee4418855d26154fc8dd850977e4df918c
liamkinne <kinneliam@gmail.com>
2018-10-24 21:29:31 +1100

    get data

--- src/main/cpp/Pixy/PixyStereo.cpp
+++ src/main/cpp/Pixy/PixyStereo.cpp
@@ -9,3 +9,8 @@ PixyStereo::PixyStereo(PixyBase *pixy_left, PixyBase *pixy_right) :
     pixy{pixy_left, pixy_right}
 {
 }
+
+std::vector<PixyBase::Block> PixyStereo::GetBlocks(uint16_t max_blocks) {
+    auto left_blocks = pixy.left->GetBlocks(max_blocks);
+    auto right_blocks = pixy.right->GetBlocks(max_blocks);
+}
--- src/main/include/Pixy/PixyStereo.h
+++ src/main/include/Pixy/PixyStereo.h
@@ -8,11 +8,12 @@ public:
     PixyStereo(PixyBase &pixy_left, PixyBase &pixy_right);
     PixyStereo(PixyBase *pixy_left, PixyBase *pixy_right);
 
+    std::vector<PixyBase::Block> GetBlocks(uint16_t max_blocks = 1000);
+
+private:
     struct {
         PixyBase *left;
         PixyBase *right;
     } pixy;
-
-private:
 };
 
