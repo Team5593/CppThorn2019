1e845ceddbd0ff6c975ef2a11c4ec43024eb3db4
Liam Kinne <liamkinne@users.noreply.github.com>
2019-01-18 06:19:42 +1100

    Remove comments

--- src/main/cpp/Commands/HumanDriver.cpp
+++ src/main/cpp/Commands/HumanDriver.cpp
@@ -1,21 +1,11 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 #include "Commands/HumanDriver.h"
 
 HumanDriver::HumanDriver() {
-  // Use Requires() here to declare subsystem dependencies
   Requires (&Robot::driveTrain);
 }
 
-// Called just before this Command runs the first time
 void HumanDriver::Initialize() {}
 
-// Called repeatedly when this Command is scheduled to run
 void HumanDriver::Execute() 
 {
   const double BASE_SPEED = 0.75;
@@ -25,12 +15,8 @@ void HumanDriver::Execute()
   Robot::driveTrain.arcadeDrive(forwards * throttle, heading * throttle);
 }
 
-// Make this return true when this Command no longer needs to run execute()
 bool HumanDriver::IsFinished() { return false; }
 
-// Called once after isFinished returns true
 void HumanDriver::End() {}
 
-// Called when another command which requires one or more of the same
-// subsystems is scheduled to run
 void HumanDriver::Interrupted() {}
